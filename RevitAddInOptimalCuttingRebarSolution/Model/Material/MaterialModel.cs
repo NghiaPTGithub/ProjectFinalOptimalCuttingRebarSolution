a845dd7af252783f2c390602959f71e1d8b38d63
nghiaphamhc <99626412+nghiaphamhc@users.noreply.github.com>
2022-04-09 17:26:34 +0700

    220409

---
+++ RevitAddInOptimalCuttingRebarSolution/Model/Material/MaterialModel.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAddInOptimalCuttingRebarSolution.Model.Material
+{
+    public class MaterialModel
+    {
+        #region Property
+        public string Id;
+        public string Name;
+        public Element ElementRevit;
+        public FamilyInstance FamilyInstance;
+        public Family Family;
+        public Category Category;
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Method
+        #endregion
+    }
+}
