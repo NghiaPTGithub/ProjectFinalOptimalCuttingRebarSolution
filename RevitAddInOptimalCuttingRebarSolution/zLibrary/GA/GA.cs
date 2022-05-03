2297b3fb0d555aeb0a1e0616f31cd65098c08a0b
nghiaphamhc <99626412+nghiaphamhc@users.noreply.github.com>
2022-05-03 22:16:49 +0700

    update

--- RevitAddInOptimalCuttingRebarSolution/zLibrary/GA/GA.cs
+++ RevitAddInOptimalCuttingRebarSolution/zLibrary/GA/GA.cs
@@ -61,6 +61,7 @@ namespace RevitAddInOptimalCuttingRebarSolution.zLibrary.GA
                     child.Add(motherItem);
                 }
             }
+
             return child;
         }
     }
